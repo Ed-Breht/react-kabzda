bccd9570baf84942d18a02d8aa958fdd33379379
Ed-Breht <Bowman488@mail.ru>
2020-10-13 22:17:01 +0300

    Initial commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+    return  <div className='content'>
+        <div>
+            <img src='https://krot.info/uploads/posts/2020-01/1580225726_56-p-foni-dlya-rieltorskikh-saitov-119.jpg' />
+        </div>
+        <div>
+            ava+description
+        </div>
+        <div>
+            my posts
+            <div>
+                new post
+            </div>
+        </div>
+        <div>
+            post 1
+        </div>
+        <div>
+            post 2
+        </div>
+    </div>
+}
+
+export default  Profile;
