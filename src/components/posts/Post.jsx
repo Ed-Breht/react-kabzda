5a8c94f95030b27c5555faaa361baa7d42beff3c
Ed-Breht <Bowman488@mail.ru>
2020-10-23 00:24:54 +0300

    hz

--- src/components/posts/Post.jsx
+++ src/components/posts/Post.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React from 'react'
 import styles from './Post.module.scss'
 
-const Post = ({message}) => {
-    return (
-        <div className={styles.post}>
-            <img
-                src='https://smolensk-i.ru/wp-content/uploads/2019/08/Kubik-Rubika-ulitsa-Nikolaeva-ulitsa-Marinyi-Raskovoy-foto-facebook.com-baburchenkov.jpg'/>
-            {message}
-            <div>
-                <span> Like </span>
-            </div>
-        </div>)
+const Post = ({ message }) => {
+  return (
+    <div className={styles.post}>
+      <img
+        alt={'avatar'}
+        src="https://smolensk-i.ru/wp-content/uploads/2019/08/Kubik-Rubika-ulitsa-Nikolaeva-ulitsa-Marinyi-Raskovoy-foto-facebook.com-baburchenkov.jpg"
+      />
+      {message}
+      <div>
+        <span> Like </span>
+      </div>
+    </div>
+  )
 }
 
-export default Post;
+export default Post
